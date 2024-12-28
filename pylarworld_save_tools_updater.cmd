9b30564e851b06678f3f2f0b468d22f4d0f6f504
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2024-12-27 21:42:24 -0800

    Update pylarworld_save_tools_updater.cmd

--- pylarworld_save_tools_updater.cmd
+++ pylarworld_save_tools_updater.cmd
@@ -1,21 +1,11 @@
 @echo off
 title Pylar's Automated PalWorldSaveTools Updater
-set "current_dir=%cd%"
-if exist "PalWorldSaveTools" (
-    echo Updating PalWorldSaveTools...
-    cd PalWorldSaveTools
-    git pull
-    cd ..
-) else (
-    echo Cloning PalWorldSaveTools to a temporary directory...
-    git clone https://github.com/deafdudecomputers/PalWorldSaveTools.git Temp_PalWorldSaveTools
-)
-echo Replacing files in the current directory...
-xcopy "Temp_PalWorldSaveTools\*" "%current_dir%\" /E /H /C /Y
-:: Clean up the temporary folder if used
-if exist "Temp_PalWorldSaveTools" (
-    echo Cleaning up temporary files...
-    rmdir /S /Q "Temp_PalWorldSaveTools"
-)
-echo Update complete.
+git init >nul 2>&1
+git remote remove origin >nul 2>&1
+git remote add origin https://github.com/deafdudecomputers/PalWorldSaveTools.git
+echo Replacing all files in the current directory with the latest from GitHub...
+git fetch --all
+git reset --hard origin/main
+git clean -fdx
+echo Update complete. All files have been replaced.
 pause
