de182f57b9d3ce07ee04d2a348884482f0cd0f5c
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2024-12-28 15:04:21 -0800

    venv's

---
+++ fix_host_save_venv.cmd
@@ -0,0 +1,15 @@
+@echo off
+title Pylar's Save Tool *venv*
+echo Setting up your environment!
+
+:: Create a virtual environment
+python -m venv venv
+
+:: Activate the environment
+call venv\Scripts\activate
+
+:: Install the required packages
+python -m pip install -r requirements.txt
+
+:: Run the fix_save.cmd
+.\fix_host_save.cmd
