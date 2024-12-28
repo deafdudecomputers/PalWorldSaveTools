d537eb387f082b58f9476d191636d0ae682844cf
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2024-12-28 15:57:24 -0800

    Update fix_save.cmd

--- fix_save.cmd
+++ fix_save.cmd
@@ -14,34 +14,4 @@ python -m pip install -r requirements.txt
 :: Run the fix_save.py with Level.sav
 python fix_save.py Level.sav
 
-:: Check if Level.sav was found
-if exist "Level.sav" (
-    echo Would you like to delete saves of:
-    echo 1. Both
-    echo 2. Pals
-    echo 3. Players
-    echo 4. None
-    set /p choice="Enter the number of your choice: "
-    if "!choice!"=="1" (
-        echo Executing delete_pals_save.cmd...
-        call delete_pals_save.cmd
-        echo Executing sort_players.cmd...
-        call sort_players.cmd
-    ) else if "!choice!"=="2" (
-        echo Executing delete_pals_save.cmd...
-        call delete_pals_save.cmd
-    ) else if "!choice!"=="3" (
-        echo Executing sort_players.cmd...
-        call sort_players.cmd
-    ) else if "!choice!"=="4" (
-        echo Exiting.
-        exit /b 0
-    ) else (
-        echo Invalid choice. Exiting.
-        exit /b 1
-    )
-) else (
-    echo Level.sav does not exist. Skipping choices.
-)
-
 pause
