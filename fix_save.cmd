03916192a92dfcec20c6e58d9602924cdcca0f78
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2024-12-29 02:56:31 -0800

    Fixed a bug

--- fix_save.cmd
+++ fix_save.cmd
@@ -11,6 +11,13 @@ call venv\Scripts\activate
 :: Install the required packages
 python -m pip install -r requirements.txt
 
+:: Delete old files from previous runs
+if exist "fix_save.log" del "fix_save.log"
+if exist "players.log" del "players.log"
+if exist "sort_players.log" del "sort_players.log"
+if exist "Pal Logger" rmdir /s /q "Pal Logger"
+if exist "import_lock.txt" del "import_lock.txt"
+
 :: Run the fix_save.py with Level.sav
 python fix_save.py Level.sav
 
