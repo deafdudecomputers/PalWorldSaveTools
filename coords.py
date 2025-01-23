ca96c18d5df595b3f9340c4022f30193525fd1b1
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2025-01-23 15:19:50 -0800

    Update coords.py

--- coords.py
+++ coords.py
@@ -1,22 +1,43 @@
-import math
-def convert_coordinates():
-    transl_x = 123888
-    transl_y = 158000
-    scale = 459
-    print("--- Convert .sav to In-Game Coordinates ---")
-    sav_x = int(input("Enter .sav X coordinate: "))
-    sav_y = int(input("Enter .sav Y coordinate: "))
-    temp_x = sav_x + transl_x
-    temp_y = sav_y - transl_y
-    in_game_x = round(temp_y / scale)
-    in_game_y = round(temp_x / scale)
-    print(f"In-game coordinates: X = {in_game_x}, Y = {in_game_y}\n")
-    print("--- Convert In-Game to .sav Coordinates ---")
-    game_x = int(input("Enter in-game X coordinate: "))
-    game_y = int(input("Enter in-game Y coordinate: "))
-    temp_x_sav = game_x * scale
-    temp_y_sav = game_y * scale
-    sav_x_out = round(temp_y_sav - transl_x)
-    sav_y_out = round(temp_x_sav + transl_y)
-    print(f".sav coordinates: X = {sav_x_out}, Y = {sav_y_out}\n")
-convert_coordinates()
+from collections import namedtuple
+__transl_x = 123888
+__transl_y = 158000
+__scale = 459
+Point = namedtuple('Point', ['x', 'y'])
+def get_number_in_range(min_value, max_value):
+    while True:
+        try:
+            number = int(input(f"Enter a number between {min_value} and {max_value}: "))
+            if min_value <= number <= max_value:
+                return number
+            else:
+                print("Number is out of range. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+def sav_to_map(x: float, y: float) -> Point:
+    newX = x + __transl_x
+    newY = y - __transl_y
+    return Point(x=round(newY/__scale), y=round(newX/__scale))
+def map_to_sav(x: int, y: int) -> Point:
+    newX = x * __scale
+    newY = y * __scale
+    return Point(x=newY - __transl_x, y=newX + __transl_y)
+def main():
+    print("1. Convert .sav to in-game")
+    print("2. Convert in-game to .sav")
+    intUserChoice = get_number_in_range(1, 2)
+    if intUserChoice == 1:
+        try:
+            sav_x = int(input("Enter Sav X coordinate: "))
+            sav_y = int(input("Enter Sav Y coordinate: "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+        print(sav_to_map(sav_x ,sav_y))
+    if intUserChoice == 2:
+        try:
+            game_x = int(input("Enter In-game X coordinate: "))
+            game_y = int(input("Enter In-game Y coordinate: "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+        print(map_to_sav(game_x ,game_y))
+if __name__ == "__main__":
+    main()
