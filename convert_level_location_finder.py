7ce47c90239bcc82cd2f5cc8b5a561cc86c5ae72
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2025-01-23 21:26:44 -0800

    Update convert_level_location_finder.py

--- convert_level_location_finder.py
+++ convert_level_location_finder.py
@@ -1,5 +1,4 @@
-from convert import *
-import glob, sys
+from convert import *; import os, sys
 def search_file(pattern, directory): return glob.glob(f"{directory}/**/{pattern}", recursive=True)
 def main():
     if len(sys.argv) != 2 or sys.argv[1] not in ["sav", "json"]: print("Usage: script.py <sav|json>"); exit(1)
