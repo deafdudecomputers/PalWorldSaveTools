0ca3bed6712157d810b3f5ce575fb60bf8d99658
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2025-01-27 02:34:23 -0800

    v1.0.16.beta

--- internal_libs/import_libs.py
+++ internal_libs/import_libs.py
@@ -3,7 +3,7 @@ external_libs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..
 os.makedirs(external_libs_path, exist_ok=True)
 sys.path.insert(0, external_libs_path)
 import argparse, code, collections, copy, ctypes, datetime, functools, gc, importlib.metadata, json, shutil, glob, requests
-import logging, multiprocessing, platform, pprint, re, subprocess, tarfile, threading, cProfile, pickle, zipfile
+import logging, multiprocessing, platform, pprint, re, subprocess, tarfile, threading, cProfile, pickle, zipfile, customtkinter
 import time, traceback, uuid, io, pathlib, tkinter as tk, tkinter.font, pstats, hashlib, csv, urllib.request, tempfile
 from multiprocessing import shared_memory
 from tkinter import ttk, filedialog, messagebox
